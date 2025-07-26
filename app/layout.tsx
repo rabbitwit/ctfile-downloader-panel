63f6f33eae7b3dbfae7a72325796230b8103d27b
Nekohy <qaq3254865169@gmail.com>
2025-07-26 15:06:11 +0800

    Change the title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: '城通网盘下载器',
+  description: 'Build with love by Nekohy',
+  generator: 'github.com/Nekohy',
 }
 
 export default function RootLayout({
